84ab13e747a6116e8e56b1f63f21e4a82d67cf09
thanhpham312 <tpham74@my.bcit.ca>
2018-02-10 16:53:55 -0800

    Fixed contants.py.

--- constants.py
+++ constants.py
@@ -6,7 +6,6 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREY = (115, 115, 115)
 
-<<<<<<< HEAD
 SCREEN_WIDTH = 360
 SCREEN_HEIGHT = 640
 PIXEL = 5
@@ -17,12 +16,10 @@ SHOOTER_WIDTH = 50
 SHOOTER_HEIGHT = 50
 
 
-=======
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 640
 PIXEL = 5
 
->>>>>>> d25bf3f0537c1ff76e9934740aef4a5eb2c2dce5
 LOOP_FREQUENCY = 150
 
 FOOD = ["./assets/food/good/", "./assets/food/bad/"]
