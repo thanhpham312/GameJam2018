63284e65cbb340a6b487ffdc419d3d574c441bd3
AHuang170 <aldrichhuang321@gmail.com>
2018-02-10 17:26:04 -0800

    more stuffs

--- game_level.py
+++ game_level.py
@@ -8,14 +8,14 @@ class GameLevel:
     def __init__(self, screen):
         self.targets = []
         self.screen = screen
-        self.create_target()
     
     def create_target(self, x_coor=0):
         new_target = Target(self.screen, x_coor)
         self.targets.append(new_target)
     
     def update_state(self, tick):
-        if len(self.targets) < 10 and tick == 60:
+
+        if len(self.targets) < 10 and tick == TARGET_UPDATE_DELAY:
             self.create_random_target()
 
         for entry in self.targets:
@@ -24,5 +24,5 @@ class GameLevel:
                 self.targets.remove(entry)
 
     def create_random_target(self):
-        random_x_coor = random.randint(0,SCREEN_WIDTH - 100)
+        random_x_coor = random.randint(0, SCREEN_WIDTH - 100)
         self.create_target(random_x_coor)
