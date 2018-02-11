823fd8c12d3dbf2469cf31f537401ae002a4adbc
AHuang170 <aldrichhuang321@gmail.com>
2018-02-10 16:44:38 -0800

    more stuff

--- constants.py
+++ constants.py
@@ -6,23 +6,13 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREY = (115, 115, 115)
 
-SCREEN_WIDTH = 960
-SCREEN_HEIGHT = 540
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 640
 PIXEL = 5
-
-# Shooter & bullets:
-SHOOTER_SPEED = 0.1
-SHOOTER_WIDTH = 50
-SHOOTER_HEIGHT = 50
-
 
 LOOP_FREQUENCY = 150
 
-BUILDING_HEIGHT_SCALE = 2
-
-BOMB_LIMIT = 20
-BOMB_EXPLOSION_DELAY = 5
-BASE_BOMB_EXPLOSION_DURATION = 5
-PLANE_TOP_SPEED_LIMIT = 20
-PLANE_DELAY_LIMIT = 20
-PLANE_INITIAL_UPDATE_DELAY = 2
+FOOD = ["./assets/food/good/", "./assets/food/bad/"]
+TARGETS = ["apple", "aubergine", "broccoli", "carrot", "cherries", "chili", "grapes", "lemon", "orange", "pineapple",
+           "raspberry", "salad", "strawberry", "tomato", "watermelon"]
+IMG_EXT = ".png"
--- target.py
+++ target.py
@@ -1,6 +1,6 @@
 import pygame
+import random
 from constants import *
-
 
 class Target:
     __X_OFFSET = 0
@@ -10,10 +10,12 @@ class Target:
         self.x_coor = x_coor
         self.y_coor = 0
         self.screen = screen
+
+        img_file = self.get_random_img()
         
-        self.apple_img = pygame.image.load('apple.png')
+        self.apple_img = pygame.image.load(img_file[0])
         self.apple = pygame.transform.scale(self.apple_img, (100, 100))
-        self.apple_erase_img = pygame.image.load('apple_erase.png')
+        self.apple_erase_img = pygame.image.load(img_file[1])
         self.apple_erase = pygame.transform.scale(self.apple_erase_img, (100, 100))
         
         self.draw()
@@ -27,7 +29,13 @@ class Target:
     def move(self):
         self.x_coor += Target.__X_OFFSET
         self.y_coor += Target.__Y_OFFSET
-    
+
+    def get_random_img(self):
+        food_list = TARGETS
+        random.shuffle(food_list)
+
+        return FOOD[1] + food_list[0] + IMG_EXT, FOOD[1] + food_list[0] + "_erase" + IMG_EXT
+
     def update_target_state(self):
         self.erase()
         self.move()
