5fc36b5a3b1514edcb42921223a67aa181f308d3
AHuang170 <aldrichhuang321@gmail.com>
2018-02-10 16:19:23 -0800

    ajfkldsa

--- game_level.py
+++ game_level.py
@@ -1,3 +1,5 @@
+import random
+
 from constants import *
 from target import Target
 
@@ -8,10 +10,19 @@ class GameLevel:
         self.screen = screen
         self.create_target()
     
-    def create_target(self):
-        new_target = Target(self.screen)
+    def create_target(self, x_coor=0):
+        new_target = Target(self.screen, x_coor)
         self.targets.append(new_target)
     
-    def update_state(self):
+    def update_state(self, tick):
+        if len(self.targets) < 5 and tick == 60:
+            self.create_random_target()
+
         for entry in self.targets:
             entry.update_target_state()
+            if entry.end:
+                self.targets.remove(entry)
+
+    def create_random_target(self):
+        random_x_coor = random.randint(0,SCREEN_WIDTH - 100)
+        self.create_target(random_x_coor)
--- target.py
+++ target.py
@@ -3,28 +3,18 @@ from constants import *
 
 
 class Target:
-    __X_OFFSET = 1
+    __X_OFFSET = 0
     __Y_OFFSET = 1
     
-    def __init__(self, screen):
-        self.x_coor = 0.5 * SCREEN_WIDTH
+    def __init__(self, screen, x_coor=0.5*SCREEN_WIDTH):
+        self.x_coor = x_coor
         self.y_coor = 0
         self.screen = screen
         
         self.apple_img = pygame.image.load('apple.png')
         self.apple = pygame.transform.scale(self.apple_img, (100, 100))
-<<<<<<< HEAD
-
-        self.apple_erase_img = pygame.image.load('./assets/food/apple_erase.png')
+        self.apple_erase_img = pygame.image.load('apple_erase.png')
         self.apple_erase = pygame.transform.scale(self.apple_erase_img, (100, 100))
-
-        self.apple_box = pygame.Surface((100, 100))
-        self.apple_box.fill(WHITE)
-
-=======
-        self.apple_box = pygame.Surface((100, 100))
-        self.apple_box.fill(SUNSET_BACKDROP)
->>>>>>> parent of 3c9ae5b... s
         
         self.draw()
         
@@ -32,13 +22,17 @@ class Target:
         self.screen.blit(self.apple, (self.x_coor, self.y_coor))
         
     def erase(self):
-        self.screen.blit(self.apple_box, (self.x_coor, self.y_coor))
+        self.screen.blit(self.apple_erase, (self.x_coor, self.y_coor))
     
     def move(self):
         self.x_coor += Target.__X_OFFSET
         self.y_coor += Target.__Y_OFFSET
     
     def update_target_state(self):
-        # self.erase()
+        self.erase()
         self.move()
         self.draw()
+
+    @property
+    def end(self):
+        return self.y_coor > SCREEN_HEIGHT
